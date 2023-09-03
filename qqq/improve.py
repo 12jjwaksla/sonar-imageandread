569070869a30ae23d231e3bdcdb830f327a658d1
12jjwaksla <tzsuo202021@163.com>
2023-09-03 19:57:07 +0800

    11

--- qqq/improve.py
+++ qqq/improve.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #请注意，请在树莓派启动的时候 将这个程序设置为自启动
 import socket
 import PIL
@@ -13,4 +14,21 @@ tcp_socket.connect(server_addr)
 tcp_socket.send('$GPOTH,256,*75\r\n')#发送启动声呐软件的命令
 
 # 4. 关闭套接字
+=======
+#请注意，请在树莓派启动的时候 将这个程序设置为自启动
+import socket
+import PIL
+# 1.创建socket
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 2. 链接服务器
+server_addr = ("192.168.0.144", 7000)
+tcp_socket.connect(server_addr)
+
+# 3. 发送数据
+
+tcp_socket.send('$GPOTH,256,*75\r\n')#发送启动声呐软件的命令
+
+# 4. 关闭套接字
+>>>>>>> b8368ea (tijiao)
 tcp_socket.close()
--- qqq/test1.py
+++ qqq/test1.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from PIL import Image
 import math
 import binascii
@@ -5,4 +6,13 @@ f=open('E:\\桌面\\大型auv声呐实验\\sonardata\\0816 202747image.txt','r')
 d=f.readline()
 d1=d[29:-2]
 print(d1)
+=======
+from PIL import Image
+import math
+import binascii
+f=open('E:\\桌面\\大型auv声呐实验\\sonardata\\0816 202747image.txt','r')
+d=f.readline()
+d1=d[29:-2]
+print(d1)
+>>>>>>> b8368ea (tijiao)
 print(len(d1))
